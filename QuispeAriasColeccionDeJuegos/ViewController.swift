981f43c5c5a475671bb6404fc17387cc06d91a8d
KatherineMilagros <katherine.quispr@tecsup.edu.pe>
2024-06-08 23:00:53 -0500

    Initial Commit

---
+++ QuispeAriasColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuispeAriasColeccionDeJuegos
+//
+//  Created by Katherine Quispe Arias on 8/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
